2561f49d730dbcd0630872b1d5f0cda116848e38
Guilherme Fujii <gui.fujii@outlook.com>
2021-08-04 20:49:24 -0300

    initial commit

---
+++ src/test/java/haoc/fiap/healthbackend/HealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package haoc.fiap.healthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
