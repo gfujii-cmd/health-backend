ae5ed1eb1208f022024dc198393fbb893765414c
Guilherme Fujii <gui.fujii@outlook.com>
2021-09-16 21:58:40 -0300

    new: entity for badges

--- src/main/java/haoc/fiap/healthbackend/entity/Badge.java
+++ src/main/java/haoc/fiap/healthbackend/entity/Badge.java
@@ -1,12 +1,16 @@
 package haoc.fiap.healthbackend.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Table(name = "BADGES")
 public class Badge {
@@ -15,6 +19,9 @@ public class Badge {
     @Column(name = "BADGE_ID")
     private Integer id;
 
-    @Column(name = "DESCRIPTION")
+    @Column(name = "BADGE_DESCRIPTION")
     private String description;
+
+    @Column(name = "BADGE_LEVEL")
+    private String level;
 }
